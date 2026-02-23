0d1288d9531cb90ba3c6358775fad826047f4015
Khang Pham <phamthekhang632@gmail.com>
2026-02-23 21:38:59 +0900

    feat: keymap to open and close nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,6 +2,10 @@ return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
   lazy = false,
+  keys = {
+    { "<leader>to", function() require("nvim-tree.api").tree.open() end, desc = "Open nvim-tree" },
+    { "<leader>tc", function() require("nvim-tree.api").tree.close() end, desc = "Close nvim-tree" },
+  },
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
